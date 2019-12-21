ad528cb79f616772d7bc82ab25f97bbfba8865ab
mee-kell <54895264+mee-kell@users.noreply.github.com>
2019-12-21 19:57:35 +0800

    Add files via upload

---
+++ code-blue/Code Blue/Code Blue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code Blue
+//
+//  Created by Michelle Lo on 21/12/19.
+//  Copyright © 2019 Michelle Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ code-blue/README.md
@@ -0,0 +1,2 @@
+# code-blue
+A mental health app for YC students. (CAS Project 2020)
