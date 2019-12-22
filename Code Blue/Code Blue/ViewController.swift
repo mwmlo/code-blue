8983c5ba5f18c29713c5f037eb4128921fe8b7ec
Michelle <michelle.lo14@sec.ycis-hk.com>
2019-12-22 14:54:43 +0800

    Added about page.

--- Code Blue/Code Blue/ViewController.swift
+++ Code Blue/Code Blue/ViewController.swift
@@ -15,6 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func timeManagementVector(_ sender: Any) {
+        if let url = URL(string: "https://www.vecteezy.com/free-vector/time-management") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    
+    @IBAction func peopleTalkingVector(_ sender: Any) {
+        if let url = URL(string: "https://www.vecteezy.com/free-vector/people-talking") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    
+    @IBAction func mentalHealthVector(_ sender: Any) {
+        if let url = URL(string: "https://www.vecteezy.com/free-vector/mental-health") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    
+    @IBAction func givingVector(_ sender: Any) {
+        if let url = URL(string: "https://www.vecteezy.com/free-vector/giving") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
 }
-
